6342614aca6ab9be072482316df0532c2d3a2693
Pedro Trujillo <pedroetb@gmail.com>
2024-08-01 13:29:29 +0100

    Migra desde yarn v1 hacia npm, ajusta CI

--- grunt/config/buildModules.js
+++ grunt/config/buildModules.js
@@ -1,10 +1,8 @@
 module.exports = function(grunt) {
 
-	var preBuildCmds = ['yarn install'];
-
 	grunt.config('redmicConfig.buildModules', {
-		'dep/pruneCluster': preBuildCmds.concat([
+		'dep/pruneCluster': [
 			'grunt uglify'
-		])
+		]
 	});
 };
