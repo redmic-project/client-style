2d21ed27dd4e6dcea9fd2770e48e9c06b8fe0921
Pedro Trujillo <pedroetb@gmail.com>
2024-08-13 09:11:06 +0100

    Instala subdependencias antes de construir

--- grunt/config/buildModules.js
+++ grunt/config/buildModules.js
@@ -1,8 +1,10 @@
 module.exports = function(grunt) {
 
+	var preBuildCmds = ['npm install'];
+
 	grunt.config('redmicConfig.buildModules', {
-		'dep/pruneCluster': [
+		'dep/pruneCluster': preBuildCmds.concat([
 			'grunt uglify'
-		]
+		])
 	});
 };
